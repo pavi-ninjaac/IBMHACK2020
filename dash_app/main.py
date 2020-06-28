26d31b7b1b0dc1d99db1e96cfa861b2d33e0ca89
PaviNinjac <51699297+pavi-ninjaac@users.noreply.github.com>
2020-06-28 23:57:33 +0530

    updated map and hover data maps

--- dash_app/main.py
+++ dash_app/main.py
@@ -12,6 +12,9 @@ from dash.dependencies import Input,Output
 import pandas as pd
 from tweetwrangling import tweets_analyse
 
+import json
+import plotly.express as px
+import plotly.graph_objects as go
 
 total_confirmed,sorted_confirmed,total_deaths,sorted_deaths,total_recovered,sorted_recovered,total_active,sorted_active=tweets_analyse.get_data()
 time_series_df=tweets_analyse.time_series_creation()
@@ -26,7 +29,7 @@ total_name=['    -  Global confirmed','     -   Global Death','     -   Global R
 
 #sorted cases among the country
 
-
+#generate scrolabel data for death and confirm cases
 def generate_tabel(df,title):
     return html.Table([
 
@@ -38,7 +41,52 @@ def generate_tabel(df,title):
             ])
         ])
 
+
+#map creation
+    
+#access token from mapbox https://account.mapbox.com/
+token='pk.eyJ1IjoibmluamFhYyIsImEiOiJja2J5bjB4YmQwaXg2MzBuNHNzaTA0bXk2In0.BVaAwyDqBSPCCYqRFQBdcA'   
+
+fig = px.scatter_mapbox(time_series_df, 
+                        lat="Lat", lon="Long", 
+                        hover_name="Country/Region", 
+                        hover_data=['Country/Region'],
+                        zoom=1,
+                        color_discrete_sequence=["fuchsia"],
+                        height=500)
+
+fig.update_layout(mapbox_style="dark",mapbox_accesstoken=token)
+
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+
+#time series map ceration based on the map cover data
+colors={
+    'backgroud':"black",
+    'text':'gray',
+}
+def create_time_series(df,country_name,col_name,title):
+    fig=dict(
+        data=[
+            {'x':list(df['date']), 'y':list(df[col_name]),
+             'type' : 'bar' ,
+             'name' : 'Date',
+             
+                 },
+            ],
+        layout={'title' : country_name +'\t' + title,
+                
+                'plot_bgcolor':colors['backgroud'],
+                'paper_bgcolor':colors['backgroud'],
+                'font' :{
+                    'color':colors['text']  
+                    }
+                },
+        )
+    return fig
+
+
 app=dash.Dash(__name__)
+
 
 app.layout=html.Div(children=[
                     html.Div(className='header',
@@ -47,18 +95,24 @@ app.layout=html.Div(children=[
                                       
 
                     html.Div(className='first_box',
-                             style={'width':'17%','display':'inline-block','float':'left'},
+                             style={'width':'17%',
+                                    'display':'inline-block',
+                                    'float':'left'},
                              children=[html.Ol(id='total_list',children=[html.Li(html.Div(style={'height':'30px'},className='list',children=[html.H2(i),html.H6(j)])) for i,j in zip(total,total_name)])
                                        ]),
                     
                     html.Div(className='confirm_countrywise',
-                             style={'width':'17%','display':'inline-block,','float':'right'},
+                             style={'width':'17%',
+                                    'display':'inline-block,',
+                                    'float':'right'},
                              children=[generate_tabel(sorted_confirmed,"Total Confirmed cases"),
                                        
                                        ]),
 
                     html.Div(className='sentiment_emotion_graph',
-                             style={'width':'64%','display':'inline-block','float':'right',},
+                             style={'width':'64%',
+                                    'display':'inline-block',
+                                    'float':'right',},
                              children=[html.Div('ssdfghb')]),
 
                     html.Div(className='death_countrywise',
@@ -70,9 +124,66 @@ app.layout=html.Div(children=[
                              style={"width":"17%",},
                              children=[html.Div('Death Basedon Country')]),
                                        
-                    html.Div('esrxdcfghbjnbhvgcfvhbvcfgv')
+                    html.Div(className='map',
+                             style={'width':'60%',
+                                    'display':'inline-block',
+                                    'height':'5%',
+                                    'float':'left'},
+                             children=[
+                                
+                                     dcc.Graph(
+                                             id='scatter_world_map',
+                                             hoverData={'points':[{'customdata':['India']}]},
+                                             figure=fig,
+            )
+                                 ]),
+                    html.Div(className='time_series_map',
+                             style={'dispaly':'inline-block',
+                                    'width':'38%',
+                                    "float":'right'},
+                             children=[
+                                 dcc.Graph(
+                                     id='death_time_series_map',
+                                     ),
+                                 dcc.Graph(
+                                     id='conifrm_time_series_map',
+                                     )
+                                 ]),
+                   
                         
                         ])
+
+"""
+@app.callback(Output(component_id='check', component_property='children'),
+              [Input(component_id='scatter_world_map',component_property='hoverData')])
+def display_hover_data(hoverData):
+    return json.dumps(hoverData, indent=2)
+
+"""
+@app.callback(Output(component_id='death_time_series_map', component_property='figure'),
+              [Input(component_id='scatter_world_map',component_property='hoverData')])
+def ubdate_death_time_series(hoverData):
+    #full=full_table_f[full_table_f['Country/Region'] == 'Afghanistan']
+    country_name = hoverData['points'][0]['customdata'][0]
+    time_df=time_series_df[time_series_df['Country/Region'] == country_name]
+    time_df_death=time_df[['Death','date']]
+    col_name='Death'
+    title='Death rate Date wise'
+    return create_time_series(time_df_death,country_name,col_name,title)
+    
+
+@app.callback(Output(component_id='conifrm_time_series_map', component_property='figure'),
+              [Input(component_id='scatter_world_map',component_property='hoverData')])
+def ubdate_confirm_time_series(hoverData):
+    #full=full_table_f[full_table_f['Country/Region'] == 'Afghanistan']
+    country_name = hoverData['points'][0]['customdata'][0]
+    time_df=time_series_df[time_series_df['Country/Region'] == country_name]
+    time_df_death=time_df[['Confirmed','date']]
+    col_name='Confirmed'
+    title='Confirm cases Date wise'
+    return create_time_series(time_df_death,country_name,col_name,title)
+   
+
   
 if __name__=='__main__':
     app.run_server(debug=True)
