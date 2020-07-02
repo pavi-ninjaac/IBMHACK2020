13f0b07fa6eaec3fa4f3e7f69b4fa2c3b354ecb0
PaviNinjac <51699297+pavi-ninjaac@users.noreply.github.com>
2020-07-03 00:04:43 +0530

    trying to get word frequency

--- dash_app/node_red_data_collection.py
+++ dash_app/node_red_data_collection.py
@@ -3,12 +3,16 @@
 Created on Tue Jun 30 11:41:47 2020
 
 @author: ninjaac
+
 """
+import pandas as pd
+import numpy as np
+
+  
 class sentiment_data():
-    
+
     def get_sentiment_data():
-        import pandas as pd
-        import numpy as np
+
         #read the files
         tweet=pd.read_csv(r'F:\dash_app\dash_ibm_app\dataset\tweet.txt',sep='\n')
         tweet.columns=['tweets']
@@ -17,14 +21,36 @@ class sentiment_data():
         emotion=pd.read_csv(r'F:\dash_app\dash_ibm_app\dataset\emotion.csv')
         emotion.columns=['emotion']
         
+        #future emotions and sentiment of people
+        future_sentiment=pd.read_csv(r'F:\dash_app\dash_ibm_app\dataset\sentiment.csv')
+        future_sentiment.columns=['sentiment']
+        
         #prepare the dataframe stuiable for pie chart
         sentiment_values=list(sentiment['sentiment'].value_counts())
         sentimet_names=['positive','negative','neutral']
         
         emotion_value=emotion['emotion'].value_counts()
-        print(emotion_value)
         emotion_name=['fear','angery','joy','disgust','sadness']
-        return sentiment_values,sentimet_names,emotion_value,emotion_name
-
         
+        future_sentiment_value=list(future_sentiment['sentiment'].value_counts())
+        future_sentimet_names=['positive','negative','neutral']
+        
+        #total values 
+        sentiment_total=sentiment.shape[0]
+        emotion_total=emotion.shape[0]
+        future_sentiment_total=future_sentiment.shape[0]
+        
+        total=[sentiment_total,emotion_total,future_sentiment_total]
+        
+        return sentiment_values,sentimet_names,emotion_value,emotion_name,future_sentiment_value,future_sentimet_names,total
 
+    def get_frequent_word():
+        tweet=pd.read_csv(r'F:\dash_app\dash_ibm_app\dataset\tweet.txt',sep='\n')
+        tweet.columns=['tweets']
+        
+        count=tweet.tweets.str.split(expand=True).stack().value_counts()
+        count=[word for word in count]
+        word=tweet.tweets.str.split(expand=True).stack().value_counts().index.tolist()
+        df=pd.DataFrame({'word':word,'count':count})
+        
+        
